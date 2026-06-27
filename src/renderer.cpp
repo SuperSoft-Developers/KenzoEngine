dcc66abc6cf50e540525cbd5d9062b249b2e085c
SuperSoft-Developers <begameguidi@gmail.com>
2026-06-27 20:29:36 -0300

    Update renderer.cpp

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -14,10 +14,39 @@ Renderer::~Renderer()
 
 bool Renderer::Init()
 {
-    std::cout << "[Renderer] Inicializado." << std::endl;
+    std::cout << "[Renderer] Inicializado com sucesso." << std::endl;
     return true;
 }
 
 void Renderer::Clear()
 {
-    //
+    // Limpa a tela (implementação futura)
+}
+
+void Renderer::DrawSprite(const std::string& image, int x, int y)
+{
+    std::cout << "[Sprite] "
+              << image
+              << " X=" << x
+              << " Y=" << y
+              << std::endl;
+}
+
+void Renderer::DrawText(const std::string& text, int x, int y)
+{
+    std::cout << "[Texto] \""
+              << text
+              << "\" X=" << x
+              << " Y=" << y
+              << std::endl;
+}
+
+void Renderer::Present()
+{
+    // Atualiza a tela (implementação futura)
+}
+
+void Renderer::Shutdown()
+{
+    std::cout << "[Renderer] Finalizado." << std::endl;
+}
